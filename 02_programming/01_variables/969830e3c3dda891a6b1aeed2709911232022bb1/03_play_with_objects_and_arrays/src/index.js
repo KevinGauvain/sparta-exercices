96f2ede5fb30f6b6d861163a97e7f41a544fb4c6
Kevin Gauvain <kevin.gauvain@decathlon.com>
2022-01-11 15:17:54 +0100

    exoPlayWithObjectsAndArrayTerminer

---
+++ 02_programming/01_variables/969830e3c3dda891a6b1aeed2709911232022bb1/03_play_with_objects_and_arrays/src/index.js
@@ -0,0 +1,16 @@
+const question = "Spartan, what is your profession?!";
+const answer = "Dev dev dev!";
+
+// a constant called `sparta`, bound to an object with a field `check`, with the value of `question` and with the field `response`, with the value of `answer`;
+const sparta = {
+  check : question ,
+  response : answer
+};
+console.log(sparta);
+
+// a constant called `leader`, bound to the value of the field `check` of the object `sparta`;
+const leader = sparta.check ;
+console.log(leader) ;
+
+// a constant called `digits`, bound to an array from zero to nine.
+const digits = [0,1,2,3,4,5,6,7,8,9] ;
