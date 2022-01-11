f01efe260144e44d3a30ed1ba2f5546a3152775b
Kevin Gauvain <kevin.gauvain@decathlon.com>
2022-01-11 15:00:09 +0100

    exoPlayWithNumbersTerminerCorriger

--- 02_programming/01_variables/969830e3c3dda891a6b1aeed2709911232022bb1/02_play_with_numbers/src/index.js
+++ 02_programming/01_variables/969830e3c3dda891a6b1aeed2709911232022bb1/02_play_with_numbers/src/index.js
@@ -1,11 +1,12 @@
 // a constant called `foo`, bound to an integer: 12;
 const foo = 12 ;
-
+console.log(foo);
 // a constant called `bar`, bound to an integer: 28;
 const bar = 28 ;
-
+console.log(bar);
 // a constant called `sumResult`, bound to the sum of `foo` and `bar`;
 const sumResult = foo + bar ;
-
+console.log(sumResult) ;
 // a constant called `prodResult`, bound to the product of `bar` and `foo`.
 const prodResult = bar * foo ;
+console.log(prodResult) ;
