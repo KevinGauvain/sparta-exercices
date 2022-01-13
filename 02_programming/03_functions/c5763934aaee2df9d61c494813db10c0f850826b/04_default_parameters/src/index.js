95267c19298812ea604f198e963a8af02c71b2e6
Kevin Gauvain <kevin.gauvain@decathlon.com>
2022-01-13 17:10:29 +0100

    exoDefaultParametersTerminer

---
+++ 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/04_default_parameters/src/index.js
@@ -0,0 +1,20 @@
+function helloSpartan(name = "Hello soldier; not very talkative uh?") {
+  // Code the function here.
+  if (name === "Hello soldier; not very talkative uh?"){
+    console.log("Hello soldier; not very talkative uh?");}
+  else if (name === "soldier"){
+    console.log("Hello soldier; not very talkative uh?");
+  }
+  else {console.log(`Hello ${name}: glad to know your name!`);
+  }}
+
+// Do not remove last lines, it is for tests
+module.exports = helloSpartan;
+
+function helloSpartan(name = "soldier") {
+  // Code the function here.
+  if (name === "soldier"){
+    console.log("Hello soldier; not very talkative uh?");}
+  else {console.log(`Hello ${name}: glad to know your name!`);
+  }}
+  
