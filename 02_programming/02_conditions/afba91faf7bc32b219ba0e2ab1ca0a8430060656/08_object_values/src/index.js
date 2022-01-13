8b9837768be9c9e6ee58b2dfc178adf782c2cbdf
Kevin Gauvain <kevin.gauvain@decathlon.com>
2022-01-13 11:07:21 +0100

    exoObjectValuesTerminer

--- 02_programming/02_conditions/afba91faf7bc32b219ba0e2ab1ca0a8430060656/08_object_values/src/index.js
+++ 02_programming/02_conditions/afba91faf7bc32b219ba0e2ab1ca0a8430060656/08_object_values/src/index.js
@@ -11,14 +11,18 @@ const odinRequest = "Mjöllnir";
 
 // Code here a constant armouryWithNamesOnly bound to an array called that has all the values of the object armoury
 // using the object method to get all values (google is your friend)
-
+const armouryWithNamesOnly = Object.values(armoury);
 
 
 // Create a constant isTheRequestInArmoury bound to the check of the presence
 // of odinRequest in armouryWithNamesOnly
-
+const isTheRequestInArmoury = armouryWithNamesOnly.includes(odinRequest);
 
 
 // Create a condition that logs "Ah, I finally found something to hammer that nail into"
 // if the isTheRequestInArmoury is true and "What do you want me to do with this?" if not
-
+if(isTheRequestInArmoury){
+  console.log("Ah, I finally found something to hammer that nail into");
+} else {
+  console.log("What do you want me to do with this?");
+}
