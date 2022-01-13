56c935a1a5a9b1883bd5a2e65c291c46032cff1f
Kevin Gauvain <kevin.gauvain@decathlon.com>
2022-01-13 17:07:09 +0100

    exoFortuneTellerTerminer

---
+++ 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/02_fortune_teller/src/index.js
@@ -0,0 +1,6 @@
+function tellFortune(job, city, name, years) {
+  // Code the function here.
+  console.log(`You will be ${job} in ${city}, and married to ${name} in ${years} years.`);
+}
+// Do not remove last lines, it is for tests
+module.exports = tellFortune;
