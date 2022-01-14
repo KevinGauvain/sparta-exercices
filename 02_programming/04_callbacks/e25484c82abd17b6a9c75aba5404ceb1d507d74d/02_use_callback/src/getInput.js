74478d9664ef3266f0283ab20d6d6bdabac8b23f
Kevin Gauvain <kevin.gauvain@decathlon.com>
2022-01-14 15:31:48 +0100

    exoUseCallbackTerminer

---
+++ 02_programming/04_callbacks/e25484c82abd17b6a9c75aba5404ceb1d507d74d/02_use_callback/src/getInput.js
@@ -0,0 +1,7 @@
+// A function getInput that takes two parameters, an input (the first one) and a callback function
+
+const getInput = (user, getInput) => { // code the function here
+  return getInput(user.key);
+};
+// Leave the line below for tests to work
+module.exports = getInput;
