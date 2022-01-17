ef731f1a7cc88845fcb7e9a6aa762e5976facb4b
Kevin Gauvain <kevin.gauvain@decathlon.com>
2022-01-17 10:06:06 +0100

    exoFunctionsMaj

--- 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/05_filtering_roles/src/index.js
+++ 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/05_filtering_roles/src/index.js
@@ -1,7 +1,23 @@
-function filteredRoles(){
+function filteredRoles(person, role = "Teacher"){
   // Code the function here.
-
+mySortedArray = [
+  {
+    name: "John",
+    role: "Teacher",
+  },
+  {
+    name: "Marie",
+    role: "Teacher",
+  },
+  {
+    name : "Sacha",
+    role : "Student"
+  },
+  {
+    name :"Lorie",
+    role : "Teacher"
+  }
+];
 }
-
 // Do not remove lines below, it is for tests
 module.exports = filteredRoles;
