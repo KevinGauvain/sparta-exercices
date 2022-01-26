6ccf3a380de26c84b3d296c9d01eb1da66cb86eb
Kevin Gauvain <kevin.gauvain@decathlon.com>
2022-01-26 09:29:37 +0100

    updateExoMaj25Janvier

--- 02_programming/02_conditions/afba91faf7bc32b219ba0e2ab1ca0a8430060656/06_conditional_statement_drink/src/index.js
+++ 02_programming/02_conditions/afba91faf7bc32b219ba0e2ab1ca0a8430060656/06_conditional_statement_drink/src/index.js
@@ -2,8 +2,30 @@
 
 // Create two objects, `frieda`, `francis` with the fields :
 //   `age` (an integer) and `name`, (obviously a string)
+const frieda = {
+  name: "Frieda",
+  age: 22,
+};
 
-
+const francis = {
+  name: "Francis",
+  age: 17,
+};
 
 // Create two variables `canFriedaDrinkAlcohol` and `canFrancisDrinkAlcohol` to answer,
 // respectively, to the question "Can he / she drink alcohol?".
+let canFriedaDrinkAlcohol;
+
+if (frieda.age >= 18) {
+  canFriedaDrinkAlcohol = true;
+} else {
+  canFriedaDrinkAlcohol = false;
+}
+
+let canFrancisDrinkAlcohol;
+
+if (francis.age >= 18) {
+  canFrancisDrinkAlcohol = true;
+} else {
+  canFrancisDrinkAlcohol = false;
+}
