743bc3a42cb39215365135b7b353087bb5d166df
Kevin Gauvain <kevin.gauvain@decathlon.com>
2022-01-26 09:25:10 +0100

    updateExo

--- 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/05_filtering_roles/src/index.js
+++ 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/05_filtering_roles/src/index.js
@@ -1,23 +1,53 @@
-function filteredRoles(person, role = "Teacher"){
+function filteredRoles(persons, role = "Teacher") {
   // Code the function here.
-mySortedArray = [
-  {
-    name: "John",
-    role: "Teacher",
-  },
-  {
-    name: "Marie",
-    role: "Teacher",
-  },
-  {
-    name : "Sacha",
-    role : "Student"
-  },
-  {
-    name :"Lorie",
-    role : "Teacher"
+  const listOfPersons = [
+    {
+      name: "Frieda",
+      role: "Teacher",
+    },
+    {
+      name: "John",
+      role: "Teacher",
+    },
+    {
+      name: "Francis",
+      role: "Student",
+    },
+    {
+      name: "Jane",
+      role: "Student",
+    },
+  ];
+
+  const ArrayFilterSpecificRole = [];
+  if (role === listOfPersons[0].role) {
+    ArrayFilterSpecificRole.push({
+      name: listOfPersons[0].name,
+      role: listOfPersons[0].role,
+    });
   }
-];
+  if (role === listOfPersons[1].role) {
+    ArrayFilterSpecificRole.push({
+      name: listOfPersons[1].name,
+      role: listOfPersons[1].role,
+    });
+  }
+  if (role === listOfPersons[2].role) {
+    ArrayFilterSpecificRole.push({
+      name: listOfPersons[2].name,
+      role: listOfPersons[2].role,
+    });
+  }
+  if (role === listOfPersons[3].role) {
+    ArrayFilterSpecificRole.push({
+      name: listOfPersons[3].name,
+      role: listOfPersons[3].role,
+    });
+  }
+  return ArrayFilterSpecificRole;
 }
+
+filteredRoles(["Frieda", "John", "Francis", "Jane"], "Teachers");
+
 // Do not remove lines below, it is for tests
 module.exports = filteredRoles;
--- 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/06_preparing_a_spacecraft/src/index.js
+++ 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/06_preparing_a_spacecraft/src/index.js
@@ -1,9 +1,27 @@
-function checkFuelLevel() {
+function checkFuelLevel(integer) {
   // Code here
+  if (integer > 20) {
+    return "Fuel level: green";
+  } else if (integer > 10) {
+    return "Fuel level: yellow";
+  } else {
+    return "Fuel level: red";
+  }
 }
 
-function checkCargoHold(){
+function checkCargoHold(array) {
   // Code here
+  const newArray = array.length;
+  if (newArray === 4) {
+    return "Full";
+  } else if (newArray < 4) {
+    const spaceAvailable = 4 - newArray;
+    return `Spaces available: ${spaceAvailable}`;
+  } else if (newArray > 4) {
+    const overCapacity = newArray - 4;
+    return `Over capacity by ${overCapacity} items.`;
+  }
+  return;
 }
 
 // Do not remove last lines, it is for tests
