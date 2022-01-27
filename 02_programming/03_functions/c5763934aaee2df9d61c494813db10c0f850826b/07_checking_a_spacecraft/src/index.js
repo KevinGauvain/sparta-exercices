d6297afa16383f6cbda20daadeb183f89340a2a6
Kevin Gauvain <kevin.gauvain@decathlon.com>
2022-01-27 09:41:16 +0100

    updateExoMaj

--- 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/07_checking_a_spacecraft/src/index.js
+++ 02_programming/03_functions/c5763934aaee2df9d61c494813db10c0f850826b/07_checking_a_spacecraft/src/index.js
@@ -1,4 +1,25 @@
 // Code here
+const stealthyFuelLevelCheck = (number) => {
+  if (number > 20) {
+    return "Fuel level: green";
+  } else if (number > 10 && number < 20) {
+    return "Fuel level: yellow";
+  } else {
+    return "Fuel level: red";
+  }
+};
+
+const stealthyCargoHoldCheck = (array) => {
+  if (array.length === 4) {
+    return "Full";
+  } else if (array.length < 4) {
+    const spaceRemaining = 4 - array.length;
+    return `Spaces available: ${spaceRemaining}`;
+  } else if (array.length > 4) {
+    const overCapacity = array.length - 4;
+    return `Over capacity by ${overCapacity} items.`;
+  }
+};
 
 // Do not remove last lines, it is for tests
 
