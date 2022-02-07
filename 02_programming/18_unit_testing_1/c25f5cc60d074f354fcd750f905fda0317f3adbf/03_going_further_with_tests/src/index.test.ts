5798482881d090ea54379ffa5959b7ff0bbe0d0f
Kevin Gauvain <kevin.gauvain@decathlon.com>
2022-02-07 09:50:57 +0100

    updateExo

--- 02_programming/18_unit_testing_1/c25f5cc60d074f354fcd750f905fda0317f3adbf/03_going_further_with_tests/src/index.test.ts
+++ 02_programming/18_unit_testing_1/c25f5cc60d074f354fcd750f905fda0317f3adbf/03_going_further_with_tests/src/index.test.ts
@@ -47,44 +47,94 @@ describe("Let the tests on arrays begin !", () => {
       "Tombeau des lucioles",
       "fight club",
     ];
-    expect.assertions(mockArray.length).toBe(5); ?????????????????????
+    const sizeList = mockArray.length;
+    expect(sizeList).toBeGreaterThanOrEqual(5);
   });
 
   // Create a test that checks if the array only contains String element.
   // Remember that a test is code, you are not limited to using `expect`.
-  mockArray = [
-    "Lord of the rings : the return of the king",
-    "Into the wild",
-    "Alien the 8th passenger",
-    "Tombeau des lucioles",
-    "fight club",
-  ];
+  test("Only strings in the array", function () {
+    expect.assertions(1);
+    mockArray = [
+      "Lord of the rings : the return of the king",
+      "Into the wild",
+      "Alien the 8th passenger",
+      "Tombeau des lucioles",
+      "fight club",
+    ];
+    var test = true;
+    for (let i = 0; i < mockArray.length; i++) {
+      if (typeof mockArray[i] === "string") {
+        test = true;
+      } else {
+        test = false;
+        break;
+      }
+    }
+    expect(test).toBe(true);
+  });
 
   // Create a test that passes if there's at least one film with "Lord of the rings" inside mockArray.
-  mockArray = [
-    "Lord of the rings : the return of the king",
-    "Into the wild",
-    "Alien the 8th passenger",
-    "Tombeau des lucioles",
-    "fight club",
-  ];
+  test("Lord of th rings is in mockArray", function () {
+    expect.assertions(1);
+    mockArray = [
+      "Lord of the rings : the return of the king",
+      "Into the wild",
+      "Alien the 8th passenger",
+      "Tombeau des lucioles",
+      "fight club",
+    ];
+
+    const test = [];
+    for (let i = 0; i < mockArray.length; i++) {
+      if (typeof mockArray[i] === "string") {
+        test.push(mockArray[i]);
+      }
+    }
+
+    let result;
+    for (let i = 0; i < test.length; i++) {
+      if (test[i].includes("Lord of the rings") === true) {
+        result = true;
+        break;
+      } else {
+        result = false;
+      }
+    }
+    expect(result).toBe(true);
+  });
 
   //Create a test that checks if the elements in mockArray respect that the strings are the same with the right order as solutionArray
   //eg : mockArray = ["Blue","Blue","Red", "Yellow"] would fail the test
-  mockArray = ["Blue", "Red", "Blue", "Yellow"];
-  const solutionArray = ["Blue", "Red", "Blue", "Yellow"];
+  test("String same with right order", function () {
+    expect.assertions(1);
+
+    mockArray = ["Blue", "Red", "Blue", "Yellow"];
+    const solutionArray = ["Blue", "Red", "Blue", "Yellow"];
+
+    expect(mockArray).toEqual(solutionArray);
+  });
 });
 
 describe("Let the tests on objects begin !", () => {
   // Create a test that ensures that your 'command' object contains at least `fries: true` and `sauce: "Hannibal"` without checking properties one by one
   // You should do it with only one assertion.
   // Hint: we want our mock object to _match_ another smaller object (look at the documentation!)
-  mockObject = {
-    sauce: "Hannibal",
-    fries: true,
-    kebab: "Double",
-    soda: "Sprite",
-  };
-});
+  test("Contains fries: true and sauce: Hannibal", function () {
+    expect.assertions(1);
+    mockObject = {
+      sauce: "Hannibal",
+      fries: true,
+      kebab: "Double",
+      soda: "Sprite",
+    };
 
+    const smallerObject = {
+      sauce: "Hannibal",
+      fries: true,
+    };
+
+    expect(mockObject).toMatchObject(smallerObject);
+  });
+});
 //At the end, your file should contain 7 test (8 with the given example).
--- 02_programming/18_unit_testing_1/c25f5cc60d074f354fcd750f905fda0317f3adbf/04_coffee_recipes/src/index.ts
+++ 02_programming/18_unit_testing_1/c25f5cc60d074f354fcd750f905fda0317f3adbf/04_coffee_recipes/src/index.ts
@@ -1,3 +1,76 @@
-export function giveRecipe() {
+export function giveRecipe(coffee: string): string {
   //code here
+  type CoffeeRecipe = {
+    name: string;
+    ingredients: string[];
+  };
+
+  const coffeeRecipes: CoffeeRecipe[] = [
+    {
+      name: "espresso",
+      ingredients: ["espresso"],
+    },
+    {
+      name: "macchiato",
+      ingredients: ["espresso", "milk foam"],
+    },
+    {
+      name: "latte",
+      ingredients: ["espresso", "milk foam", "steamed milk"],
+    },
+    {
+      name: "cappuccino",
+      ingredients: ["espresso", "milk foam", "steamed milk", "chocolate"],
+    },
+    {
+      name: "americano",
+      ingredients: ["espresso", "water"],
+    },
+  ];
+
+  const coffeeNames = ["espresso", "macchiato", "latte", "cappuccino", "americano"];
+  if (coffeeNames.includes(coffee) === false) {
+    return `Sorry, ${coffee} does not exist on our list.`;
+  } else {
+    for (let i = 0; i < coffeeRecipes.length; i++) {
+      if (coffee === coffeeRecipes[i].name) {
+        return `${coffeeRecipes[i].name} ingredients:\n- ${coffeeRecipes[i].ingredients.join(",\n- ")}`;
+      }
+    }
+  }
 }
+
+giveRecipe("espresso");
+
+/*
+
+Autre -------------------------------- sans boucle
+
+type CoffeeRecipe = {
+  name: string;
+  ingredients: string[];
+};
+
+const coffeeRecipes: CoffeeRecipe[] = [
+  {
+    name: "espresso",
+    ingredients: ["espresso"],
+  },
+  {
+    name: "macchiato",
+    ingredients: ["espresso", "milk foam"],
+  },
+  {
+    name: "latte",
+    ingredients: ["espresso", "milk foam", "steamed milk"],
+  },
+  {
+    name: "cappuccino",
+    ingredients: ["espresso", "milk foam", "steamed milk", "chocolate"],
+  },
+  {
+    name: "americano",
+    ingredients: ["espresso", "water"],
+  },
+];
+*/
